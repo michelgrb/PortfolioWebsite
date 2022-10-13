0ad9c0a6072195de96af09bd6b4a9d2105564710
mlgcode <mlgerbeth@outlook.com>
2022-10-13 11:36:29 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./components/header/Header";
+const App = () => {
+  return (
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
+  );
+};
+
+export default App;
---
+++ src/components/experiance/Experiance.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./experiance.css";
+
+function Experiance() {
+  return <div>Experiance</div>;
+}
+
+export default Experiance;
