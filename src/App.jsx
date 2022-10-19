cbeff2922a8bce2a04ea1589358e2492e60df363
mlgcode <mlgerbeth@outlook.com>
2022-10-19 08:28:04 +0200

    update Testemonials/ App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import Experiance from "./components/experiance/Experiance";
+import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+
 const App = () => {
   return (
     <React.Fragment>
       <Header />
+      <Nav />
+      <About />
+      <Experiance />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </React.Fragment>
   );
 };
