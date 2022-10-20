69b132993680654ed7ec332a120c68b6d691fd37
mlgcode <mlgerbeth@outlook.com>
2022-10-20 15:00:34 +0200

    add button to Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "./header.css";
+import CTA from "./CTA";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="container header__container">
+      <h5>Hello I´m</h5>
+      <h1>Michel</h1>
+      <h5 className="text-light">Student @TUM</h5>
+      <CTA />
+    </div>
+  );
 };
 
 export default Header;
